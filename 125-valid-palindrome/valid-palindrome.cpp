4b5a4839ded695317b227abedfe364168644648d
Medha Mathur <130782967+MedhaMathur31@users.noreply.github.com>
2025-09-05 18:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (79.47%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        s.erase(remove_if(s.begin(), s.end(), [](char c) {
-        return !std::isalnum(c);
-    }), s.end());
-    cout<<s<<endl;
+        //APPROACH 2
+    //     transform(s.begin(), s.end(), s.begin(), ::tolower);
+    //     s.erase(remove_if(s.begin(), s.end(), [](char c) {
+    //     return !std::isalnum(c);
+    // }), s.end());
+    // cout<<s<<endl;
 
-    string rs = s;
-    reverse(rs.begin(), rs.end());
-    for(int i=0; i<s.size(); i++){
-        if(rs[i]!=s[i]){
-            return false;
-            break;
-        }
-    }
+    // string rs = s;
+    // reverse(rs.begin(), rs.end());
+    // for(int i=0; i<s.size(); i++){
+    //     if(rs[i]!=s[i]){
+    //         return false;
+    //         break;
+    //     }
+    // }
+
+    //APPRAOCH 1
     // for(int i=0; i<s.size(); i++){
     //     for(int j=s.size()-1; j>=0; j++){
     //         if(s[i]==s[j]){
@@ -26,6 +29,20 @@ public:
     //     }
     // }
 
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    s.erase(remove_if(s.begin(), s.end(), [](char c) {
+        return !std::isalnum(c);
+    }), s.end());
+    cout<<s<<endl;
+
+    int i=0, j=s.size()-1;
+    while(i<j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
 
     return true;
 
