36e464847f4d4338318fb96a537a6db0ca3bd641
Medha Mathur <130782967+MedhaMathur31@users.noreply.github.com>
2025-09-26 19:14:43 +0530

    Time: 431 ms (38.85%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,8 +1,5 @@
 Select 
-    DISTINCT(author_id) as id
-from 
-    Views
-Where 
-    author_id = viewer_id
-order by 
-    author_id asc;
+DISTINCT(author_id) as id
+from Views
+Where author_id = viewer_id
+order by author_id asc;
