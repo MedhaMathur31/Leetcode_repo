4696349421c5dc391cc1269d81831f17b84f72ce
Medha Mathur <130782967+MedhaMathur31@users.noreply.github.com>
2025-10-06 13:14:29 +0530

    Time: 3 ms (40.24%) | Memory: 8.5 MB (32.00%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+
+        //APPROACH 1 - Wrong answer
+        // char i = word1[0];
+        // char j = word2[0];
+        // string ans;
+        // ans.push_back(i);
+        // ans.push_back(j);
+
+        // int g;
+        // if(word1.size() >= word2.size()){
+        //     g = word1.size();
+        // }
+        // else{
+        //     g = word2.size();
+        // }
+
+        // for(int a=1; a<g; a++){
+        //     ans.push_back(i);
+        //     ans.push_back(j);
+        //     if(i < word1.size()){
+        //         i=word1[a];
+        //     }
+        //     else continue;
+        //     if(j < word2.size()){
+        //         j = word2[a];
+        //     }
+        //     else continue;
+        // }   
+
+        int i=0, j=0;
+        string ans;
+        // ans.push_back(word1[0]);
+        // ans.push_back(word2[0]);
+
+        while(i < word1.size() || j < word2.size()){
+            if(i < word1.size()){
+                ans.push_back(word1[i++]);
+            }
+            if(j < word2.size()){
+                ans.push_back(word2[j++]);
+            }
+        }
+
+
+    return ans;
+    }
+};
